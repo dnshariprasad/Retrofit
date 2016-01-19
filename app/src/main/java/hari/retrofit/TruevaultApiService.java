e54242655be9b38a8807c747051594a7336db7dd
HariKvana <hari@kvana.com>
2016-01-19 23:12:22 +0530

    added files

--- app/src/main/java/hari/retrofit/TruevaultApiService.java
+++ app/src/main/java/hari/retrofit/TruevaultApiService.java
@@ -3,12 +3,17 @@ package hari.retrofit;
 import retrofit.Call;
 import retrofit.http.Field;
 import retrofit.http.FormUrlEncoded;
+import retrofit.http.GET;
 import retrofit.http.Header;
 import retrofit.http.POST;
 import retrofit.http.Path;
+import retrofit.http.Query;
 
 public interface TrueVaultApiService {
     @FormUrlEncoded
     @POST(" /v1/vaults/{vault_id}/documents")
     Call<CreateDocumentResponse> createDocument(@Header("Authorization") String authorization, @Path("vault_id") String vaultId, @Field("document") String document);
+
+    @GET(" /v1/vaults/{vault_id}/documents")
+    Call<TvListResponse> readAllDocuments(@Header("Authorization") String authorization, @Path("vault_id") String vaultId,@Query("full") boolean full);
 }
