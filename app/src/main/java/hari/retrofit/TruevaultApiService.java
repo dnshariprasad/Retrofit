3d2cba595e8e95b958f2966d129f96070bd924fc
Hari <dnshariprasad@gmail.com>
2016-01-20 21:06:45 -0800

    interface fix

--- app/src/main/java/hari/retrofit/TruevaultApiService.java
+++ app/src/main/java/hari/retrofit/TruevaultApiService.java
@@ -9,11 +9,19 @@ import retrofit.http.POST;
 import retrofit.http.Path;
 import retrofit.http.Query;
 
+/**
+ * Created by Hari on 1/20/2016.
+ */
 public interface TrueVaultApiService {
     @FormUrlEncoded
-    @POST(" /v1/vaults/{vault_id}/documents")
+    @POST("/v1/vaults/{vault_id}/documents")
     Call<CreateDocumentResponse> createDocument(@Header("Authorization") String authorization, @Path("vault_id") String vaultId, @Field("document") String document);
 
-    @GET(" /v1/vaults/{vault_id}/documents")
-    Call<TvListResponse> readAllDocuments(@Header("Authorization") String authorization, @Path("vault_id") String vaultId,@Query("full") boolean full);
+    @GET("/v1/vaults/{vault_id}/documents")
+    Call<TvListResponse> readAllDocuments(@Header("Authorization") String authorization, @Path("vault_id") String vaultId, @Query("full") boolean full);
 }
+
+
+
+
+
