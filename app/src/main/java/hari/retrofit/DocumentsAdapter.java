d3c48eb3c57e0cb8d02950ea5067d3465a323b78
Hari <dnshariprasad@gmail.com>
2016-01-22 22:09:28 -0800

    popup added

--- app/src/main/java/hari/retrofit/DocumentsAdapter.java
+++ app/src/main/java/hari/retrofit/DocumentsAdapter.java
@@ -2,11 +2,14 @@ package hari.retrofit;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
-import android.text.Layout;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -15,26 +18,52 @@ import java.util.List;
  */
 public class DocumentsAdapter extends RecyclerView.Adapter<DocumentsAdapter.ViewHolder> {
     private List<Document> list;
+    private Context context;
 
     public DocumentsAdapter(Context context, List<Document> list) {
         this.list = list;
+        this.context = context;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         Context context = parent.getContext();
         LayoutInflater layoutInflater = LayoutInflater.from(context);
-        View view = layoutInflater.inflate(R.layout.row_doc, parent,false);
+        View view = layoutInflater.inflate(R.layout.row_doc, parent, false);
         ViewHolder viewHolder = new ViewHolder(view);
         return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        Document item = list.get(position);
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        final Document item = list.get(position);
         holder.name.setText(item.getName());
         holder.phone.setText(item.getPhone());
         holder.gender.setText(item.getGender());
+        holder.more_iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popupMenu = new PopupMenu(context, holder.more_iv);
+                popupMenu.inflate(R.menu.more_main);
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.delete:
+                                Toast.makeText(context, "Delete", Toast.LENGTH_LONG).show();
+                                return true;
+                            case R.id.edit:
+                                Toast.makeText(context, "Edit", Toast.LENGTH_LONG).show();
+                                return true;
+                            default:
+                                return true;
+                        }
+
+                    }
+                });
+                popupMenu.show();
+            }
+        });
     }
 
     @Override
@@ -44,6 +73,7 @@ public class DocumentsAdapter extends RecyclerView.Adapter<DocumentsAdapter.View
 
     class ViewHolder extends RecyclerView.ViewHolder {
         TextView name, gender, phone;
+        ImageView more_iv;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -51,6 +81,7 @@ public class DocumentsAdapter extends RecyclerView.Adapter<DocumentsAdapter.View
             phone = (TextView) itemView.findViewById(R.id.phone);
             gender = (TextView) itemView.findViewById(R.id.gender);
 
+            more_iv = (ImageView) itemView.findViewById(R.id.more_iv);
         }
     }
 }
